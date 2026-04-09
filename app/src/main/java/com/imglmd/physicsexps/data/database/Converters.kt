3989df5275d8effd8f0569c9f48a14666e84e2c4
xakquti <166896320+xakquti@users.noreply.github.com>
2026-04-09 23:04:54 +0800

    конвертер для json_

--- app/src/main/java/com/imglmd/physicsexps/data/database/Converters.kt
+++ app/src/main/java/com/imglmd/physicsexps/data/database/Converters.kt
@@ -3,17 +3,18 @@ package com.imglmd.physicsexps.data.database
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.imglmd.physicsexps.domain.model.ExperimentResult
 
 class Converters {
     private val gson = Gson()
     @TypeConverter
-    fun fromMap(resultMap: Map<String, Double>): String {
-        return gson.toJson(resultMap)
+    fun fromMap(resultExp: ExperimentResult): String {
+        return gson.toJson(resultExp)
     }
 
     @TypeConverter
-    fun mapToString(resultString: String ): Map<String, Double> {
-        val type = object : TypeToken<Map<String, Double>>(){}.type
+    fun mapToString(resultString: String ): ExperimentResult {
+        val type = object : TypeToken<ExperimentResult>(){}.type
         return gson.fromJson(resultString, type)
     }
 }
--- app/src/main/java/com/imglmd/physicsexps/data/database/models/ResultEntity.kt
+++ app/src/main/java/com/imglmd/physicsexps/data/database/models/ResultEntity.kt
@@ -2,10 +2,11 @@ package com.imglmd.physicsexps.data.database.models
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.imglmd.physicsexps.domain.model.ExperimentResult
 
 @Entity("results")
 data class ResultEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int,
-    val result: Map<String, Double>
+    val result: ExperimentResult
 )
