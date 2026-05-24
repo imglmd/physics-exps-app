0c0bd1c445a7920cf63b270412c07e9cf4f11820
imglmd <dangdatmini@gmail.com>
2026-05-24 16:49:43 +0300

    ui: картинки экспериментов на полный экран

--- app/src/main/java/com/imglmd/physicsexps/presentation/screens/experiment/components/ImageViewer.kt
+++ app/src/main/java/com/imglmd/physicsexps/presentation/screens/experiment/components/ImageViewer.kt
@@ -1,9 +1,6 @@
 package com.imglmd.physicsexps.presentation.screens.experiment.components
 
 import androidx.activity.compose.BackHandler
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -13,7 +10,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
@@ -25,7 +21,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.IconButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
