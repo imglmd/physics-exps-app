f6908f184dafc7aec6b12bd104243b99bfa0ec2d
imglmd <dangdatmini@gmail.com>
2026-05-25 16:37:15 +0300

    fixfixfifixfixifpisdqhofrb quweug;eiryqwikpegyuqilwk ryhvgqwyvkirwyevo;i

--- app/src/main/java/com/imglmd/physicsexps/data/database/models/ExperimentRunEntity.kt
+++ app/src/main/java/com/imglmd/physicsexps/data/database/models/ExperimentRunEntity.kt
@@ -3,11 +3,12 @@ package com.imglmd.physicsexps.data.database.models
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlin.random.Random
 
 @Entity("experiment_runs")
 data class ExperimentRunEntity(
-    @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
+    @PrimaryKey
+    val id: Int = Random.nextInt(),
     @ColumnInfo("experiment_id")
     val experimentId: String,
     val date: Long,
