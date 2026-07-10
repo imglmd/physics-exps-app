f2f18e76d8215a9fcc6b71accc428723cc77cea0
kir <dangdatmini@gmail.com>
2026-07-10 22:45:56 +0300

    ui

--- feature/constants/src/main/java/com/imglmd/physicsexps/feature/constants/presentation/component/CategoryItem.kt
+++ feature/constants/src/main/java/com/imglmd/physicsexps/feature/constants/presentation/component/CategoryItem.kt
@@ -9,19 +9,15 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -58,12 +54,12 @@ fun CategoryItem(
             )
             Spacer(Modifier.weight(1f))
 
-            Icon(
+            /*Icon(
                 imageVector = ImageVector.vectorResource(icon),
                 contentDescription = "Изменить",
                 tint = MaterialTheme.colorScheme.primary,
                 modifier = Modifier.size(22.dp)
-            )
+            )*/
         }
         Column(
             verticalArrangement = Arrangement.spacedBy(8.dp),
