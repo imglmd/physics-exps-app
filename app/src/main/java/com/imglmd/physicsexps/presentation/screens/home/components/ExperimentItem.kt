7a07b813c460192a000f8aefa54b770c1e3b588d
kir <dangdatmini@gmail.com>
2026-07-13 13:47:48 +0300

    ui

--- app/src/main/java/com/imglmd/physicsexps/presentation/screens/home/components/ExperimentItem.kt
+++ app/src/main/java/com/imglmd/physicsexps/presentation/screens/home/components/ExperimentItem.kt
@@ -2,9 +2,10 @@ package com.imglmd.physicsexps.presentation.screens.home.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,18 +14,14 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineBreak
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 
 
@@ -36,70 +33,53 @@ fun ExperimentItem(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Box(
+    Column(
         modifier = modifier
-            .aspectRatio(1f)
             .clip(RoundedCornerShape(24.dp))
+            .border(
+                1.dp,
+                MaterialTheme.colorScheme.outlineVariant,
+               RoundedCornerShape(24.dp)
+            )
+            .background(MaterialTheme.colorScheme.surface)
             .clickable(onClick = onClick)
+            .padding(5.dp)
     ) {
-        if (previewUrl != null) {
-            AsyncImage(
-                model = previewUrl,
-                contentDescription = name,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize(),
-                placeholder = painterResource(placeholder),
-                error = painterResource(placeholder),
-                fallback = painterResource(placeholder)
-            )
-        } else {
-            Image(
-                painter = painterResource(placeholder),
-                contentDescription = name,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.fillMaxSize()
-            )
-        }
-
-        // затемнение картинки
         Box(
             modifier = Modifier
-                .matchParentSize()
-                .background(
-                    Color.Black.copy(
-                        alpha = if (isSystemInDarkTheme()) 0.15f else 0f
-                    )
-                )
-        )
-
-        Box(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
                 .fillMaxWidth()
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(
-                            Color.Transparent,
-                            Color.Black.copy(alpha = if (isSystemInDarkTheme()) 0.6f else 0.3f)
-                        )
-                    )
-                )
+                .aspectRatio(1.067f)
+                .clip(RoundedCornerShape(19.dp))
         ) {
-            Text(
-                text = name,
-                color = Color.White,
-                style = MaterialTheme.typography.titleMedium.copy(
-                    fontWeight = FontWeight.SemiBold,
-                    lineBreak = LineBreak.Paragraph
-                ),
-                textAlign = TextAlign.Start,
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(horizontal = 10.dp, vertical = 10.dp),
-                maxLines = 2,
-                overflow = TextOverflow.Ellipsis
-            )
+            if (previewUrl != null) {
+                AsyncImage(
+                    model = previewUrl,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize(),
+                    placeholder = painterResource(placeholder),
+                    error = painterResource(placeholder),
+                    fallback = painterResource(placeholder)
+                )
+            } else {
+                Image(
+                    painter = painterResource(placeholder),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
         }
+        Text(
+            text = name,
+            color = MaterialTheme.colorScheme.onSurface,
+            style = MaterialTheme.typography.titleSmall.copy(
+                fontSize = 16.sp
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 6.dp, vertical = 6.dp),
+        )
     }
 }
-
